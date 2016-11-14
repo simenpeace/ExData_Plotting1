4e00380882275eaa2be39ffb52e84f27f25369db
simenpeace <simenpeace@gmail.com>
2016-11-13 21:04:19 -0600

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -2,4 +2,7 @@ getwd()
 datalire <- read.table("household_power_consumption.txt", header= TRUE, sep =";",na.rm="?",colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
 dataliresub <- subset(datalire, Date == "1/2/2007" | Date == "2/2/2007")
 hist(dataliresub$Global_active_power, xlab = "Global Active Power(Kilowatts)", col ="red", main = "Global Active Power")
+## Save the file and close the device
+  #dev.copy(png,"plot1.png", width=480, height=480)
+  #dev.off()
 

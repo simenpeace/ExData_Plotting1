1b250e7487621b669d18c55800542a8db175a70c
simenpeace <simenpeace@gmail.com>
2016-11-13 21:05:04 -0600

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -2,4 +2,8 @@
  ##combining date and time first
   dateTime <- paste(dataliresub$Date, dataliresub$Time)
   plot(dataliresub$Global_active_power~dataliresub$dateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+## Save the file and close the device
+  #dev.copy(png,"plot2.png", width=480, height=480)
+  #dev.off()
+
   

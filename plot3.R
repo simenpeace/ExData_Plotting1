99daa8836f79e042109b705acb4ac8b5bec73e31
simenpeace <simenpeace@gmail.com>
2016-11-13 21:05:26 -0600

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -6,4 +6,9 @@ with(dataliresub, {
   })
   legend("topright", col=c("black", "red", "blue"), lwd=c(1,1,1), 
          c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+## Save the file and close the device
+  #dev.copy(png,"plot3.png", width=480, height=480)
+  #dev.off()
+
          
